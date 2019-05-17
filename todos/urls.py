b38bc854d08ae19736d389c534ccdabd9d6941a9
jiwookseo <jiwookseo.dev@gmail.com>
2019-05-17 20:37:10 +0900

    Fix wrong typo

--- todos/urls.py
+++ todos/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "todos"
 urlpatterns = [
     path('', TemplateView.as_view(template_name='todos/app.html'), name="index"),
-    path('lists/', views.lists_list, name="genres_list"),
-    path('lists/<int:pk>/', views.lists_detail, name="genres_detail"),
+    path('lists/', views.lists_list, name="lists_list"),
+    path('lists/<int:pk>/', views.lists_detail, name="lists_detail"),
     path('todos/', views.todos_list, name="todos_list"),
     path('todos/<int:pk>/', views.todos_detail, name="todos_detail"),
     path('todos/<int:pk>/details/', views.details_list, name="details_list"),
