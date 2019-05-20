ba15a7fe5f65a355d11717b80aa56aff5f819df2
jiwookseo <jiwookseo.dev@gmail.com>
2019-05-20 17:21:44 +0900

    Update Django Secret Key

--- wookslist/settings.py
+++ wookslist/settings.py
@@ -19,12 +19,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b(qzd)(j69xp+i61t1!%z^plfe26tugfgu=+a&htucs#ywbqih'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wookslist.herokuapp.com','127.0.0.1']
 
 # Application definition
 
